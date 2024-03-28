3490cf3fa31ba40277e0518186218839285089b4
panteL <sebi.stahl1811@live.com>
2024-03-28 06:37:01 +0100

    added api

---
+++ js/api.js
@@ -0,0 +1,50 @@
+function sendRequest() {
+  const apiUrl = "https://api.statev.de/req/";
+  const totalWeightEndpointLab = "factory/inventory/65ca64cb06965a9320fb010e";
+  const totalWeightEndpointCar = "factory/inventory/65ca64ca06965a9320fb0031";
+  const statusEndpoint = "factory/list";
+  const bearerTokenLab = "7YC9YM41X63SG52ZDL";
+  const bearerTokenCar = "F9UKIAKBZDHWY6H6JG";
+
+  const fetchConfig = (bearerToken) => ({
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      authorization: "Bearer " + bearerToken,
+    },
+  });
+
+  const fetchData = (endpoint, config) =>
+    fetch(apiUrl + endpoint, config)
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        return response.json();
+      })
+      .catch((error) => {
+        console.error("Fehler beim Senden der Anfrage:", error);
+      });
+
+  Promise.all([
+    fetchData(totalWeightEndpointLab, fetchConfig(bearerTokenLab)),
+    fetchData(totalWeightEndpointCar, fetchConfig(bearerTokenCar)),
+    fetchData(statusEndpoint, fetchConfig(bearerTokenCar)),
+    fetchData(statusEndpoint, fetchConfig(bearerTokenLab)),
+  ]).then(([labData, carData, carStatus, labStatus]) => {
+    const totalWeightDivLab = document.getElementById("totalWeightLab");
+    const totalWeightDivCar = document.getElementById("totalWeightCar");
+    const carStatusDiv = document.getElementById("car-status");
+    const labStatusDiv = document.getElementById("lab-status");
+
+    totalWeightDivLab.innerText = `${labData.totalWeight.toFixed(0)}/1850 KG`;
+    totalWeightDivCar.innerText = `${carData.totalWeight.toFixed(0)}/1850 KG`;
+
+    carStatusDiv.innerHTML = carStatus[0].isOpen
+      ? "<a href='#' class='fa fa-check'></a>"
+      : "<a href='#' class='fa fa-times'></a>";
+    labStatusDiv.innerHTML = labStatus[0].isOpen
+      ? "<a href='#' class='fa fa-check'></a>"
+      : "<a href='#' class='fa fa-times'></a>";
+  });
+}
